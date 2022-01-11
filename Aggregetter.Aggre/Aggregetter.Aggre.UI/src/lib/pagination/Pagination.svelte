c4117c278ed88b287aa1bfc4016b33b2abfe94d6
Matt <mathew.beldon@hotmail.co.uk>
2022-01-11 21:43:02 +0000

    [-] Removes pagination from frontend, need faster way of displaying

--- Aggregetter.Aggre/Aggregetter.Aggre.UI/src/lib/pagination/Pagination.svelte
+++ Aggregetter.Aggre/Aggregetter.Aggre.UI/src/lib/pagination/Pagination.svelte
@@ -5,20 +5,20 @@
 
 	let range;
 
-	$: {
-		range = [];
-		for (let i = 1; i <= pages; ++i) {
-			range.push(i);
-		}
-	}
+	// $: {
+	// 	range = [];
+	// 	for (let i = 1; i <= pages; ++i) {
+	// 		range.push(i);
+	// 	}
+	// }
 </script>
 
 {#if pages > 1}
 	<nav>
 		<ul class="pagination">
-			{#each range as n}
+			<!-- {#each range as n}
 				<li class="page-item" class:active={n == p}><a class="page-link" href={href(n)}>{n}</a></li>
-			{/each}
+			{/each} -->
 		</ul>
 	</nav>
 {/if}
