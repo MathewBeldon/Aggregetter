c1452ae25502ee49ba6a2228dfba593699c12176
Matt <mathew.beldon@hotmail.co.uk>
2022-02-18 21:00:33 +0000

    [~] Scaffold tests

---
+++ Aggregetter.Aggre/Aggregetter.Aggre.Application.UnitTests/Features/Providers/Queries/GetProviders/GetProvidersQueryHandlerTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aggregetter.Aggre.Application.UnitTests.Features.Providers.Queries.GetProviders
+{
+    internal class GetProvidersQueryHandlerTests
+    {
+    }
+}
