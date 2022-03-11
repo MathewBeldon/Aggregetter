9ad182da1b6e6ae437bc1ac41dd7183ebb5e12d9
Matt <mathew.beldon@hotmail.co.uk>
2022-03-11 22:01:45 +0000

    [+] Adds validation to TranslateArticle

---
+++ Aggregetter.Aggre/Aggregetter.Aggre.Application/Features/Articles/Commands/Translate/TranslateArticleCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using Aggregetter.Aggre.Application.Contracts.Persistence;
+using FluentValidation;
+using System;
+
+namespace Aggregetter.Aggre.Application.Features.Articles.Commands.Translate
+{
+    public sealed class TranslateArticleCommandValidator : AbstractValidator<TranslateArticleCommand>
+    {
+        readonly IArticleRepository _articleRepository;
+
+        public TranslateArticleCommandValidator(IArticleRepository articleRepository)
+        {
+            ArgumentNullException.ThrowIfNull(articleRepository);
+
+            _articleRepository = articleRepository;
+
+            RuleFor(article => article.ArticleId)
+                .MustAsync(async (articleId, cancellationToken) => {
+                    return await _articleRepository.GetByIdAsync(articleId, cancellationToken) is not null;
+                }).WithMessage("Article does not exist");
+        }
+    }
+}
