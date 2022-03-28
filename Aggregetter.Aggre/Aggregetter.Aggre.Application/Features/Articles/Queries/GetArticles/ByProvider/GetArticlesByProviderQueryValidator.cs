7326d0e83379089864490fe21d79ca3aa3b49deb
Matt <mathew.beldon@hotmail.co.uk>
2022-03-28 21:17:36 +0100

    [*] Modifies GetArticlesByProvider validator to check for valid provider id [*] Modifies GetArticlesByProviderAndCategory valdiator to check for valid proiver and category ids

--- Aggregetter.Aggre/Aggregetter.Aggre.Application/Features/Articles/Queries/GetArticles/ByProvider/GetArticlesByProviderQueryValidator.cs
+++ Aggregetter.Aggre/Aggregetter.Aggre.Application/Features/Articles/Queries/GetArticles/ByProvider/GetArticlesByProviderQueryValidator.cs
@@ -1,13 +1,31 @@
-﻿using Aggregetter.Aggre.Application.Services.PaginationService;
+﻿using Aggregetter.Aggre.Application.Contracts.Persistence;
+using Aggregetter.Aggre.Application.Services.PaginationService;
 using Aggregetter.Aggre.Application.Settings;
+using Aggregetter.Aggre.Domain.Entities;
+using FluentValidation;
 using Microsoft.Extensions.Options;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Aggregetter.Aggre.Application.Features.Articles.Queries.GetArticles.ByProvider
 {
     public sealed class GetArticlesByProviderQueryValidator : PaginationValidatorBase<GetArticlesByProviderQuery>
     {
-        public GetArticlesByProviderQueryValidator(IOptions<PagedSettings> settings) : base(settings)
+        private readonly IBaseRepository<Provider> _baseProviderRepository;
+        public GetArticlesByProviderQueryValidator(IBaseRepository<Provider> baseProviderRepository, IOptions<PagedSettings> settings) : base(settings)
         {
+            _baseProviderRepository = baseProviderRepository ?? throw new ArgumentNullException(nameof(baseProviderRepository));
+
+            RuleFor(query => query.ProviderId)
+                .MustAsync(async (providerId, cancellationToken) => {
+                    return await ProviderExistsAsync(providerId, cancellationToken);
+                }).WithMessage("Enter a valid provider");
+        }
+
+        private async Task<bool> ProviderExistsAsync(int providerId, CancellationToken cancellationToken)
+        {
+            return await _baseProviderRepository.CheckExistsByIdAsync(providerId, cancellationToken);
         }
     }
 }
--- Aggregetter.Aggre/Aggregetter.Aggre.Application/Features/Articles/Queries/GetArticles/ByProviderAndCategory/GetArticlesByProviderAndCategoryQueryValidator.cs
+++ Aggregetter.Aggre/Aggregetter.Aggre.Application/Features/Articles/Queries/GetArticles/ByProviderAndCategory/GetArticlesByProviderAndCategoryQueryValidator.cs
@@ -1,14 +1,45 @@
-﻿using Aggregetter.Aggre.Application.Services.PaginationService;
+﻿using Aggregetter.Aggre.Application.Contracts.Persistence;
+using Aggregetter.Aggre.Application.Services.PaginationService;
 using Aggregetter.Aggre.Application.Settings;
+using Aggregetter.Aggre.Domain.Entities;
 using FluentValidation;
 using Microsoft.Extensions.Options;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Aggregetter.Aggre.Application.Features.Articles.Queries.GetArticles.ByProviderAndCategory
 {
     public sealed class GetArticlesByProviderAndCategoryQueryValidator : PaginationValidatorBase<GetArticlesByProviderAndCategoryQuery>
     {
-        public GetArticlesByProviderAndCategoryQueryValidator(IOptions<PagedSettings> settings) : base(settings)
+        private readonly IBaseRepository<Provider> _baseProviderRepository;
+        private readonly IBaseRepository<Category> _baseCategoryRepository;
+
+        public GetArticlesByProviderAndCategoryQueryValidator(IBaseRepository<Provider> baseProviderRepository, IBaseRepository<Category> baseCategoryRepository,
+            IOptions<PagedSettings> settings) : base(settings)
         {
+            _baseProviderRepository = baseProviderRepository ?? throw new ArgumentNullException(nameof(baseProviderRepository));
+            _baseCategoryRepository = baseCategoryRepository ?? throw new ArgumentNullException(nameof(baseCategoryRepository));
+
+            RuleFor(query => query.ProviderId)
+                .MustAsync(async (providerId, cancellationToken) => {
+                    return await ProviderExistsAsync(providerId, cancellationToken);
+                }).WithMessage("Enter a valid provider");
+
+            RuleFor(query => query.CategoryId)
+                .MustAsync(async (categoryId, cancellationToken) => {
+                    return await CategoryExistsAsync(categoryId, cancellationToken);
+                }).WithMessage("Enter a valid category");
+        }
+
+        private async Task<bool> ProviderExistsAsync(int providerId, CancellationToken cancellationToken)
+        {
+            return await _baseProviderRepository.CheckExistsByIdAsync(providerId, cancellationToken);
+        }
+
+        private async Task<bool> CategoryExistsAsync(int categoryId, CancellationToken cancellationToken)
+        {
+            return await _baseCategoryRepository.CheckExistsByIdAsync(categoryId, cancellationToken);
         }
     }
 }
