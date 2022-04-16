9ae7a34230ce18e0088fc4251ddac57e4267e5bb
Matt <mathew.beldon@hotmail.co.uk>
2022-04-16 20:46:56 +0100

    [+] Adds validation for auth account

---
+++ Aggregetter.Aggre/Aggregetter.Aggre.Application/Features/Accounts/Queries/AuthenticateAccount/AuthenticateAccountQueryValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Aggregetter.Aggre.Application.Features.Accounts.Queries.AuthenticateAccount
+{
+    public sealed class AuthenticateAccountQueryValidator : AbstractValidator<AuthenticateAccountQuery>
+    {
+        public AuthenticateAccountQueryValidator()
+        {
+            RuleFor(command => command.Email)
+                .NotEmpty()
+                .WithMessage("Enter an email address");
+
+            RuleFor(command => command.Password)
+                .NotEmpty()
+                .WithMessage("Enter a password");
+        }
+    }
+}
