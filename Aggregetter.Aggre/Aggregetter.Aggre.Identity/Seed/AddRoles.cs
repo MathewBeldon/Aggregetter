f15166283461a9b42b9cfce2d02d6811bad11190
Matt <mathew.beldon@hotmail.co.uk>
2022-09-03 17:10:33 +0100

    [*] Refactor suggestion

--- Aggregetter.Aggre/Aggregetter.Aggre.Identity/Seed/AddRoles.cs
+++ Aggregetter.Aggre/Aggregetter.Aggre.Identity/Seed/AddRoles.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Aggregetter.Aggre.Identity.Enums;
+using Microsoft.AspNetCore.Identity;
 using System;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,9 +14,8 @@ namespace Aggregetter.Aggre.Identity.Seed
             foreach(Enums.Role enumRole in Enum.GetValues(typeof(Enums.Role)))
             {
                 var enumRoleName = enumRole.ToString();
-                var enumRoleNormalised = enumRoleName.ToUpper();
 
-                if (!roleManager.Roles.Any(role => role.NormalizedName == enumRoleNormalised))
+                if (!roleManager.Roles.Any(role => string.Equals(role.NormalizedName, enumRoleName, StringComparison.OrdinalIgnoreCase)))
                 {
                     await roleManager.CreateAsync(new IdentityRole(enumRoleName));
                 }
