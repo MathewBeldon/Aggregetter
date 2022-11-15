922fd018248dbe15385397e90b85da2f58328db2
Matt <mathew.beldon@hotmail.co.uk>
2022-11-15 21:19:28 +0000

    [+] Updates k6 tests

--- Aggregetter.Aggre/k6/loadtestArticleDetails.js
+++ Aggregetter.Aggre/k6/loadtestArticleDetails.js
@@ -4,7 +4,7 @@ import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.1.0/index.js';
 
 export const options = {    
     vus: 1000,
-    duration: '600s',
+    duration: '60s',
 };
 
 export default function () {
